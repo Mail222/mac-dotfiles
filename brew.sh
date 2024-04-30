4a1093dd83b86405830d92859e69a9b16194335c
Liam A <98932986+Mail222@users.noreply.github.com>
2024-04-30 13:31:38 -0600

    Create brew.sh
    added necessary packages

---
+++ brew.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+/opt/homebrew/bin/brew update
+/opt/homebrew/bin/brew install wget
+/opt/homebrew/bin/brew install neofetch
+/opt/homebrew/bin/brew install micro
+/opt/homebrew/bin/brew install --cask eloston-chromium
+/opt/homebrew/bin/brew install --cask alt-tab
+/opt/homebrew/bin/brew install spicetify-cli
+echo "FINISHED"
+exit
